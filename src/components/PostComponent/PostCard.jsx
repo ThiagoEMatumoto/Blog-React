7f838e61ed672b73adaf7a60e92867aa5904edd4
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-10-20 16:40:22 -0300

    :sparkles: feat: páginas adicionadas na aplicação

--- src/components/PostComponent/PostCard.jsx
+++ src/components/PostComponent/PostCard.jsx
@@ -3,13 +3,12 @@ import styles from "./PostCard.module.css";
 import { Link } from "react-router-dom";
 
 const PostCard = ({ post }) => {
-  console.log(post.id);
   return (
     <Link to={`/posts/${post.id}`}>
       <div className={styles.post}>
         <img
           className={styles.capa}
-          src={`./posts/${post.id}/capa.png`}
+          src={`../../../src/assets/posts/${post.id}/capa.png`}
           alt="Imagem de capa do post"
         />
         <h2 className={styles.titulo}>{post.titulo}</h2>
--- src/pages/PostPage/Post.jsx
+++ src/pages/PostPage/Post.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import styles from "./PostPage.module.css";
+import "./PostPage.css";
 import { useParams } from "react-router-dom";
+import posts from "../../json/posts.json";
+import PostHeader from "../../components/PostHeader/PostHeader";
+import Markdown from "react-markdown";
 
 const Post = () => {
   const parametros = useParams();
 
-  return <div className={styles}>Post {parametros.id}</div>;
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
+
+  if (!post) {
+    return <h1>Post n encontrado</h1>;
+  }
+
+  return (
+    <PostHeader
+      fotoCapa={`../../../src/assets/posts/${post.id}/capa.png`}
+      titulo={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <Markdown>{post.texto}</Markdown>
+      </div>
+    </PostHeader>
+  );
 };
 
 export default Post;
